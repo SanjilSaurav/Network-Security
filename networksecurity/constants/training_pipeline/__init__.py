9369b091c368c5a7d01f4c7b26f841a186fcd0c1
Saurav Kumar <saurav.0kmr@gmail.com>
2025-04-12 14:18:00 +0530

    Model training

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -48,5 +48,18 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "n_neighbors": 3,
     "weights": "uniform",
 }
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
 
 PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+SAVED_MODEL_DIR = "saved_models"
+MODEL_FILE_NAME = "model.pkl"
+
+'''
+Model Trainer relalted constant start with MODEL_TRAINER VAR NAME
+'''
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD: float = 0.05
